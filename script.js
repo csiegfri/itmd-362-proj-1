e65039356051d54bc0168a2202ab9cfbed21d30b
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-10-31 14:05:10 -0500

    adjusted script JS to remove angular

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var app = angular.module('myApp', []);
-app.controller('MainCtrl', function($scope) 
-{
-    $scope.register=function(uname, email){
-        alert("Congratulations, " + uname + ", you're in. Expect an email at " + email + " in the near future.");
-    }
-});
+Thanks=function(name, email){
+   alert("Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.");
+}
