29b798ea18a0d7d27ca8a66bdaa170a7b97bb374
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-10-31 15:05:57 -0500

    adjusted to have onsubmit function, fixed function

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-window.onload = function(
-  document.getElementById("submit").addEventListener("click", function(){
-    var name = document.getElementById("name").value;
-	var email = document.getElementById("email").value;
-	result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.";		document.getElementById("demo").innerHTML = result;
-  });
-);
+function Thanks(){
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.";
+  alert(result);  
+};
