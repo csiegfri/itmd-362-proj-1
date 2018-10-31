e1a3e2f4cdf62393a4a0192736817691ed97d70e
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-10-31 14:33:13 -0500

    fixed spacing

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 window.onload = function(
-
-	document.getElementById("submit").addEventListener("click", function(){
-		var name = document.getElementById("name").value;
-		var email = document.getElementById("email").value;
-    
-		result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.";
-	
-		document.getElementById("demo").innerHTML = result;
-	});
+  document.getElementById("submit").addEventListener("click", function(){
+    var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.";		document.getElementById("demo").innerHTML = result;
+  });
 );
