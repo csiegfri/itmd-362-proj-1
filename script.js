7598726cb7a00d600dcbdc903000e9f5554725c4
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-10-31 14:15:40 -0500

    changed alert to post to HTML

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 Thanks=function(){
-	var name = document.getElementById("name");
-	var email = document.getElementById("email");
-    alert("Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.");
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+    
+	result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.");
+	
+	document.getElementById("demo").innerHTML = result;
 }
