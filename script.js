ebd793c42ad086f4558dbe94333a4be0c0889e46
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-10-31 14:22:45 -0500

    added onload command, fixed tabs

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-Thanks=function(){
-	var name = document.getElementById("name").value;
-	var email = document.getElementById("email").value;
+window.onload = function(
+	Thanks=function(){
+		var name = document.getElementById("name").value;
+		var email = document.getElementById("email").value;
     
-	result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.";
+		result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.";
 	
-	document.getElementById("demo").innerHTML = result;
-}
+		document.getElementById("demo").innerHTML = result;
+	}
 
-document.getElementById("submit").addEventListener("click", Thanks());
+	document.getElementById("submit").addEventListener("click", Thanks());
+);
