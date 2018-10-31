bd5b121408ca4b82948000a77483a8d811a2b689
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-10-31 14:09:16 -0500

    adjusted  fuction to get element by id

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-Thanks=function(name, email){
-   alert("Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.");
+Thanks=function(){
+	var name = document.getElementById("name");
+	var email = document.getElementById("email");
+    alert("Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.");
 }
