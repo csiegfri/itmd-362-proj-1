7714a365463dee352ee644440175ca677ac9b19b
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-10-31 14:19:53 -0500

    removed extra )

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ Thanks=function(){
 	var name = document.getElementById("name").value;
 	var email = document.getElementById("email").value;
     
-	result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.");
+	result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.";
 	
 	document.getElementById("demo").innerHTML = result;
 }
